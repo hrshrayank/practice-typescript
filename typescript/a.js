a3e04c207040a9196559a7344d0961016abdd8e3
hrshrayank <hrshrayank77@gmail.com>
2024-08-20 11:43:14 +0530

    commit

---
+++ typescript/a.js
@@ -0,0 +1,8 @@
+"use strict";
+const x = "Shrayank";
+console.log("Hello" + x);
+function sum(x, y) {
+    return x + y;
+}
+const sums = sum(1, 2);
+console.log(sums);
---
+++ typescript/a.ts
@@ -0,0 +1,8 @@
+const x: string = "Shrayank";
+console.log("Hello " + x);
+
+function sum(x:number, y:number) {
+    return x + y;
+}
+const sums = sum(1, 2)
+console.log(sums)
